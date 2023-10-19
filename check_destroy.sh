78160905be44f670c8ff500b08d41609486e4d2b
achen626 <81703038+achen626@users.noreply.github.com>
2023-10-19 01:50:38 -0400

    Update check_destroy.sh

--- check_destroy.sh
+++ check_destroy.sh
@@ -4,14 +4,14 @@
 
 if [[ "$#" -ne 4 ]]
 then
-  echo "usage: check_destroy.sh [name of container] [directory of mitm logs] [external IP] [type of container (1-8)]"
+  echo "usage: check_destroy.sh [name of container] [absolute path for directory of mitm logs] [external IP] [type of container (1-8)]"
     exit 1
 fi
 
 container_destroyed=0 #Check to ensure container is not attempted to be destroyed twice
 
 # checks idle time, maximum is 30 mins
-finger_output=$(sudo lxc-attach -n "$1" -- finger admin)
+finger_output=$(sudo lxc-attach -n "$1" -- bash -c "finger admin")
 time_idle=$(echo "$finger_output" | grep -oP '(\d+ days?)? (\d+ hours?)? (\d+ minutes?)? (\d+ seconds?)? idle' | awk '{$1=$1};1') #Accessing the idle time
 ./finger_time_check.sh "$time_idle" #running script to check if 30 minutes has passed
 container_destroyed=$? #Exit code 1 means the container should be deleted, 0 means it should be left alone
@@ -26,7 +26,7 @@ time_difference=$((timestamp2 - timestamp1))
 days_difference=$((time_difference / 86400))
 
 if [ $days_difference -ge 1 ] && [ $container_destroyed -eq 0 ] #If 1 day (24 hours) has passed, container is deleted
-then   
+then
   container_destroyed=1 #Flagging that container should be destroyed
 fi
 #checks for attacker logging out, flags container to be destroyed
@@ -37,36 +37,35 @@ fi
 
 if
   [ $container_destroyed -eq 1 ] #destroying container
-then 
+then
   # Later on, we will execute scripts here to zip up log files and export them to our home directory outside of the container. The data_parser.sh script will do that after we edit it, but right now it does not work
-  data_parser.sh $2 $4 
+  data_parser.sh "$2" "$4"
 
   # We will add code to add the external IP of this container back into play so another randomly
   # created container can use it
   #
   # move ip from not free ip to free ip so it can be used for next container
-  sed -n "$3"p not_free_ip_file.txt >> free_ip_file.txt
+  exchanged_periods=$(echo "$3" | sed 's/\./\\./g')
+  sed -i "$exchanged_periods"p /home/student/not_free_ip_file.txt >> /home/student/free_ip_file.txt
   # delete line from not free ip
-  sed -i "$3d" not_free_ip_file.txt
+  sed -i "$exchanged_periods"d /home/student/not_free_ip_file.txt
 
   sudo lxc-stop -n "$1"
   sudo lxc-destroy -n "$1"
 
-  crontab -l > ~/crontemp.txt
+  sudo crontab -l > /home/student/crontemp.txt
 
 # removes this container's cronjob from the crontab
   while read -r line
   do
     if [[ $(echo "$line" | grep -c "$1") -eq 0 ]]
     then
-      echo "$line" >> ~/temp.txt
+      echo "$line" >> /home/student/temp.txt
     fi
-  done < ~/crontemp.txt
+  done < /home/student/crontemp.txt
 
-  crontab ~/temp.txt
-  rm ~/temp.txt
-  rm ~/crontemp.txt
+  sudo crontab /home/student/temp.txt
+  rm /home/student/temp.txt
+  rm /home/student/crontemp.txt
 
 fi
-
-
