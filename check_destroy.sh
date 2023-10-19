960f9b474e21a7cd01ea9167816bdc0feedcaee8
achen626 <81703038+achen626@users.noreply.github.com>
2023-10-19 15:03:22 -0400

    Update check_destroy.sh

--- check_destroy.sh
+++ check_destroy.sh
@@ -11,30 +11,34 @@ fi
 container_destroyed=0 #Check to ensure container is not attempted to be destroyed twice
 
 # checks idle time, maximum is 30 mins
-start_time=$(grep "Attacker authenticated and is inside container" "$2"/mitm.log | grep -oP '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
-last_action_time=$(grep "Attacker Keystroke" "$2"/mitm.log | tail -1 | grep -oP '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
+#start_time=$(grep "Attacker authenticated and is inside container" "$2"/mitm.log | grep -oP '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
+#echo "Start time: $start_time"
+#last_action_time=$(grep "Attacker Keystroke" "$2"/mitm.log | tail -1 | grep -oP '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}')
 # checking 24 hours maximum time to spend in container
-cur_time=$(date '+%Y-%m-%d %T.%N')
-timestamp1=$(date -d "$start_time" +%s)
-timestamp2=$(date -d "$cur_time" +%s)
-last_action_stamp=$(date -d "$last_action_time" +%s)
-time_difference=$(("$timestamp2"-"$timestamp1"))
-days_difference=$(("$time_difference"/86400))
-thirty_mins=$(("$last_action_stamp"-"$timestamp1"))
-thirty_mins=$(("$thirty_mins" / 60))
+#cur_time=$(date '+%Y-%m-%d %T.%N')
+#timestamp1=$(date -d "$start_time" +%s)
+#timestamp2=$(date -d "$cur_time" +%s)
+#last_action_stamp=$(date -d "$last_action_time" +%s)
+#time_difference=$(("$timestamp2"-"$timestamp1"))
+#days_difference=$(("$time_difference"/86400))
+#thirty_mins=$(("$last_action_stamp"-"$timestamp1"))
+#thirty_mins=$(("$thirty_mins" / 60))
 
-if [[ $days_difference -ge 1 ]] || [[ $thirty_mins -ge 30 ]] #If 1 day (24 hours) or 30 minutes has passed, container is deleted
-then
-  container_destroyed=1 #Flagging that container should be destroyed
-fi
+#if [[ $days_difference -ge 1 ]] || [[ $thirty_mins -ge 30 ]] #If 1 day (24 hours) or 30 minutes has passed, container is deleted
+#then
+#  container_destroyed=1 #Flagging that container should be destroyed
+#fi
 #checks for attacker logging out, flags container to be destroyed
-if [[ $(grep -c "Attacker closed the connection" "$2"/mitm.log) -ge 1 ]]
+if [[ $(grep -c "Attacker closed connection" "$2"/mitm.log) -ge 1 ]]
 then
+  echo "Greped"
   container_destroyed=1
 fi
 
 if [[ $container_destroyed -eq 1 ]] #destroying container
 then
+
+  echo "destroyed"
   # Later on, we will execute scripts here to zip up log files and export them to our home directory outside of the container. The data_parser.sh script will do that after we edit it, but right now it does not work
   /home/student/data_parser.sh "$2" "$4"
 
@@ -42,9 +46,10 @@ then
   # created container can use it
 
   # move ip from not free ip to free ip so it can be used for next container
-  sed -n "/D $3/p" /home/student/not_free_ip_file.txt >> /home/student/free_ip_file.txt
+  sed -n "/$1 $3/p" /home/student/not_free_ip_file.txt >> /home/student/free_ip_file.txt
   # delete line from not free ip
-  sed -i "/D $3/d" /home/student/not_free_ip_file.txt
+  echo "moving things"
+  sed -i "/$1 $3/d" /home/student/not_free_ip_file.txt
 
   sudo lxc-stop -n "$1"
   sudo lxc-destroy -n "$1"
